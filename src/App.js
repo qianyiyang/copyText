5ac16fee53759afd7ff3a98be1b64cc88279cba6
qianyiyang <qianyiyang@znzmo.com>
2025-07-16 16:58:06 +0800

    复制文本修改

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { Input,Button,List, message, Flex } from 'antd';
+import { useRef, useState } from 'react';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+
+const { TextArea } = Input;
 
 function App() {
+  const [messageApi, contextHolder] = message.useMessage();
+  const textareaRef = useRef()
+  const [listData, setListData] = useState([])
+
+  const generateText = () =>{
+    const value = textareaRef.current?.resizableTextArea?.textArea?.value || ''
+    const arr = value.split('\n').map(item => item.replace(/^\d+\.?/, '').trim()).filter(Boolean)
+    setListData(arr)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       {contextHolder}
+      <div style={{padding:'20px'}}><TextArea rows={4} ref={textareaRef} /></div>
+      <div style={{padding:'20px',paddingTop: 0}}><Button type='primary' onClick={generateText}>生成</Button></div>
+      <List
+        bordered
+        dataSource={listData}
+        renderItem={(item,index) => (
+          <List.Item>
+            <Flex style={{width: '100%'}}>
+              <div style={{marginRight:10}}>{index+1}</div>
+              <CopyToClipboard text={item}
+                onCopy={() => {
+                  messageApi.success(`第${index+1}条复制成功`)
+                }}>
+                <div style={{flex: 1}}>{item}</div>
+              </CopyToClipboard>
+            </Flex>
+          </List.Item>
+        )}
+      />
     </div>
   );
 }
